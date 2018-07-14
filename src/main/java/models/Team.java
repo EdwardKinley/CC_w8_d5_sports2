6d3246cfee92c7c826a553338676e226fe596c29
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-07-14 16:20:16 +0100

    adds Team class

---
+++ src/main/java/models/Team.java
@@ -0,0 +1,66 @@
+package models;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "teams")
+public class Team {
+
+    private int id;
+    private String name;
+    private List<Player> players;
+    private Manager manager;
+    private Competition competition;
+
+    public Team() {}
+
+    public Team(String name) {
+        this.name = name;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @OneToMany(mappedBy = "team", fetch = FetchType.LAZY)
+    public List<Player> getPlayers() {
+        return players;
+    }
+    public void setPlayers(List<Player> players) {
+        this.players = players;
+    }
+
+    @OneToOne(mappedBy = "team", fetch = FetchType.LAZY)
+    public Manager getManager() {
+        return manager;
+    }
+    public void setManager(Manager manager) {
+        this.manager = manager;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "competition_id", nullabe = false)
+    public Competition getCompetition() {
+        return competition;
+    }
+    public void setCompetition(Competition competition) {
+        this.competition = competition;
+    }
+
+
+}
