6da909619e8e0be6858c761db0d1452a43f2b20e
EdwardKinley <edward_kinley@yahoo.co.uk>
2018-07-14 16:29:47 +0100

    adds Manager class

---
+++ src/main/java/models/Manager.java
@@ -0,0 +1,46 @@
+package models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "managers")
+public class Manager {
+
+    private int id;
+    private String name;
+    private Team team;
+
+    public Manager() {}
+
+    public Manager(String name, Team team){
+        this.name = name;
+        this.team = team;
+    }
+
+    @Column (name = "id")
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @OneToOne
+    @JoinColumn(name = "team_id", nullable = false)
+    public Team getTeam() {
+        return team;
+    }
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+
+
+}
